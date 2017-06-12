f95cc86b7235535e340c12ad6489aab3a314c6db
Ben Tristem <github@bentristem.com>
2017-06-12 17:25:19 +0100

    Converted to loop structure, updating issues

--- 3-Tiling Textures/Assets/WebWorker.cs
+++ 3-Tiling Textures/Assets/WebWorker.cs
@@ -1,49 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WebWorker : MonoBehaviour {
 
-    [SerializeField] string albedoURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker+Plate_Albedo.png";
-    [SerializeField] string heightMapURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png";
-	[SerializeField] string normalMapURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png";
-	[SerializeField] string occlusionMapURL = "";
-	[SerializeField] string metallicURL = "";
+    struct ShaderProperty
+    {
+        public string imageURL;
+        public string propertyName;
+        public string keyword;
 
-	const string HEIGHT_MAP_SHADER_PROPERTY = "_ParallaxMap";
-    const string NORMAL_MAP_SHADER_PROPERTY = "_BumpMap";
-    const string OCCLUSION_MAP_SHADER_PROPERTY = "_OcclusionMap";
-
-	IEnumerator Start()
-	{
-		// Download textures
-        WWW wwwAlbedo = new WWW(albedoURL);
-		yield return wwwAlbedo;
-        WWW wwwHeightMap = new WWW(heightMapURL);
-		yield return wwwHeightMap;
-        WWW wwwNormalMap = new WWW(normalMapURL);
-		yield return wwwNormalMap;
-        WWW wwwOcclusionMap = new WWW(occlusionMapURL);
-		yield return occlusionMapURL;
-
-	
-		// Assign textures
-		Renderer myRenderer = GetComponent<Renderer>();
-        myRenderer.material.mainTexture = wwwAlbedo.texture;
-        myRenderer.material.SetTexture(HEIGHT_MAP_SHADER_PROPERTY, wwwHeightMap.texture);
-
-        var normalMap = wwwNormalMap.texture;
-        myRenderer.material.SetTexture(NORMAL_MAP_SHADER_PROPERTY, normalMap);
-
-
-        if (wwwOcclusionMap.error == null)
+        public ShaderProperty(string imageUrl, string propertyName, string keyword)
         {
-            myRenderer.material.SetTexture(OCCLUSION_MAP_SHADER_PROPERTY, wwwOcclusionMap.texture);
+            this.imageURL = imageUrl;
+            this.propertyName = propertyName;
+            this.keyword = keyword;
         }
+    }
 
-        PlayerPrefs.SetString("TextureURL", albedoURL);
+    Renderer myRenderer = null;
 
-        print(myRenderer.material.GetTexture(NORMAL_MAP_SHADER_PROPERTY));
-        print(myRenderer.material.GetTexture(OCCLUSION_MAP_SHADER_PROPERTY)) ; 
+    ShaderProperty[] shaderProperties = new ShaderProperty[5] {
+        new ShaderProperty("https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker+Plate_Albedo.png", "_MainTex", ""),
+        new ShaderProperty("https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png", "_ParallaxMap", "_PARALLAXMAP"),
+        new ShaderProperty("https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png", "_BumpMap", "_NORMALMAP"),
+		new ShaderProperty("", "_OcclusionMap", ""),
+		new ShaderProperty("", "_MetallicGlossMap", "_METALLICGLOSSMAP"),
+    };
+
+    IEnumerator Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        foreach (ShaderProperty property in shaderProperties)
+        {
+            WWW www = new WWW(property.imageURL);
+            yield return www;
+
+            if (www.error == null)
+            {
+                var map = www.texture;
+				print("Assigining " + property.propertyName);
+                myRenderer.material.SetTexture(property.propertyName, map);
+                if (property.keyword != "")
+                {
+                    myRenderer.material.EnableKeyword(property.keyword);
+                    print("Enabling " + property.keyword); 
+                }
+            }
+        }
+        myRenderer.UpdateGIMaterials();
+		print("Finished");
+    }
+
+    public static Texture2D ToNormalMap2(Texture2D bumpTexture)
+    {
+        Texture2D normalMap = new Texture2D(bumpTexture.width, bumpTexture.height, TextureFormat.ARGB32, true);
+
+		for (int y = 0; y < bumpTexture.height; y++)
+		{
+			for (int x = 0; x < bumpTexture.width; x++)
+			{
+                float xLeft = bumpTexture.GetPixel(x - 1, y).grayscale;
+				float xRight = bumpTexture.GetPixel(x + 1, y).grayscale;
+				float yUp = bumpTexture.GetPixel(x, y - 1).grayscale;
+				float yDown = bumpTexture.GetPixel(x, y + 1).grayscale;
+                float xDelta = ((xLeft - xRight) + 1) * 0.5f;
+                float yDelta = ((yUp - yDown) + 1) * 0.5f;
+
+				normalMap.SetPixel(x, y, new Color(xDelta, yDelta, 1.0f, 1.0f));
+			}
+		}
+		normalMap.Apply();
+        return normalMap;
 	}
 }
