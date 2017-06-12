78b329ffbbebe97a33f4790cd3fdac332f85fdb6
Ben Tristem <github@bentristem.com>
2017-06-12 15:52:12 +0100

    More maps importing, struggling with normals

--- 3-Tiling Textures/Assets/WebWorker.cs
+++ 3-Tiling Textures/Assets/WebWorker.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class WebWorker : MonoBehaviour {
 
-	[SerializeField] string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+    [SerializeField] string albedoURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker+Plate_Albedo.png";
+    [SerializeField] string heightMapURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png";
+	[SerializeField] string normalMapURL = "https://s3-us-west-2.amazonaws.com/sharemygame.com/texture-test/Checker%20Plate_Heightmap.png";
+	[SerializeField] string occlusionMapURL = "";
+	[SerializeField] string metallicURL = "";
+
+	const string HEIGHT_MAP_SHADER_PROPERTY = "_ParallaxMap";
+    const string NORMAL_MAP_SHADER_PROPERTY = "_BumpMap";
+    const string OCCLUSION_MAP_SHADER_PROPERTY = "_OcclusionMap";
 
 	IEnumerator Start()
 	{
-		// Start a download of the given URL
-		WWW www = new WWW(url);
+		// Download textures
+        WWW wwwAlbedo = new WWW(albedoURL);
+		yield return wwwAlbedo;
+        WWW wwwHeightMap = new WWW(heightMapURL);
+		yield return wwwHeightMap;
+        WWW wwwNormalMap = new WWW(normalMapURL);
+		yield return wwwNormalMap;
+        WWW wwwOcclusionMap = new WWW(occlusionMapURL);
+		yield return occlusionMapURL;
 
-		// Wait for download to complete
-		yield return www;
-
-		// assign texture
+	
+		// Assign textures
 		Renderer myRenderer = GetComponent<Renderer>();
-		myRenderer.material.mainTexture = www.texture;
+        myRenderer.material.mainTexture = wwwAlbedo.texture;
+        myRenderer.material.SetTexture(HEIGHT_MAP_SHADER_PROPERTY, wwwHeightMap.texture);
 
-        PlayerPrefs.SetString("TextureURL", url);
+        var normalMap = wwwNormalMap.texture;
+        myRenderer.material.SetTexture(NORMAL_MAP_SHADER_PROPERTY, normalMap);
+
+
+        if (wwwOcclusionMap.error == null)
+        {
+            myRenderer.material.SetTexture(OCCLUSION_MAP_SHADER_PROPERTY, wwwOcclusionMap.texture);
+        }
+
+        PlayerPrefs.SetString("TextureURL", albedoURL);
+
+        print(myRenderer.material.GetTexture(NORMAL_MAP_SHADER_PROPERTY));
+        print(myRenderer.material.GetTexture(OCCLUSION_MAP_SHADER_PROPERTY)) ; 
 	}
 }
