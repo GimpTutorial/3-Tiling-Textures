21a119e24a2ae0701a9a3633c19f29093e6723d0
Ben Tristem <github@bentristem.com>
2017-06-12 14:51:27 +0100

    Down to one sphere, reading texture from web

---
+++ 3-Tiling Textures/Assets/WebWorker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebWorker : MonoBehaviour {
+
+	[SerializeField] string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+
+	IEnumerator Start()
+	{
+		// Start a download of the given URL
+		WWW www = new WWW(url);
+
+		// Wait for download to complete
+		yield return www;
+
+		// assign texture
+		Renderer myRenderer = GetComponent<Renderer>();
+		myRenderer.material.mainTexture = www.texture;
+
+        PlayerPrefs.SetString("TextureURL", url);
+	}
+}
